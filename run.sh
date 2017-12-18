08253114c607f2d91b4795604247d9dc7a6b34f7
Phillip Lehner <pl@philliplehner.com>
2017-12-18 14:21:37 +0000

    Adds logging for bash script

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 until $(/usr/bin/node /home/pi/Code/sesame-hw/sesame.js); do
 	echo "Sesame Client has crashed with exit code $?. Respawning... " >&2
+	echo "$(date) :: Sesame Client has crashed with exit code $?. Respawning... " >> sesame-run.log
 	sleep 1
 done
