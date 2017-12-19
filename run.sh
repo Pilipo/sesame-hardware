8374e05098a9a609360b3f3cf9831087ef4c8f31
Phillip Lehner <pl@philliplehner.com>
2017-12-19 21:22:35 +0000

    Refactors a lot of the garage watcher and the function surrounding it.

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-until $(/usr/bin/node /home/pi/Code/sesame-hw/sesame.js); do
+DIR=/home/pi/Code/sesame-hw
+
+mv $DIR/sesame.log $DIR/sesame-$(date +%N).log
+touch $DIR/sesame.log
+
+until $(/usr/bin/node $DIR/sesame.js >> sesame-run.log); do
+	mv $DIR/sesame.log $DIR/sesame-$(date +%N).log
+	touch $DIR/sesame.log
 	echo "Sesame Client has crashed with exit code $?. Respawning... " >&2
 	echo "$(date) :: Sesame Client has crashed with exit code $?. Respawning... " >> sesame-run.log
 	sleep 1
--- sesame.js
+++ sesame.js
@@ -1,55 +1,130 @@
+// INCLUDE DOT ENV TO OBSCURE SENSATIVE DATA
 require('dotenv').config({path: '/home/pi/Code/sesame-hw/.env'});
+// INCLUDE BLYNK AND ONOFF TO CONTROL THE PINS
 var Blynk = require('blynk-library');
 var Gpio = require('onoff').Gpio;
+// INCLUDE CMD LINE INTERFACE
 var cmd = require('node-cmd');
+// INCLUDE EVENT WATCHER ON THE MOUSE AT EVENT0
 var ie = require('input-event');
 var input = new ie('/dev/input/event0');
 var mouse = new ie.Mouse(input);
+// INCLUDE LOGGER FOR DEBUG AND DATA GATHERING
 const log = require('simple-node-logger').createSimpleFileLogger('/home/pi/Code/sesame-hw/sesame.log');
-mouse.x = mouse.y = 0;
 
+// INIT THE BLYNK CLIENT
 var AUTH = process.env.AUTH;
 var blynk = new Blynk.Blynk(AUTH, options = {
     connector : new Blynk.TcpClient( options = { addr:process.env.IP, port:8442 } )
 });
 
+// SET TO TRUE TO ENABLE DOOR ACTIVATION
+var ARMED = false;
+
+//***********************************
+// INIT A FEW PINS.
+//***********************************
+// VIRTUAL 0:   TIED TO THE "LOCK" BUTTON ON THE APP
+// VIRTUAL 1:   TIED TO THE "ACTIVATOR" BUTTON ON THE APP
+// VIRTUAL 10:  TIED TO THE CAMERA MODE SELETION ON THE APP
+// VIRTUAL 11:  TIED TO THE "TAKE PICTURE" BUTTON ON THE APP
+// LEDLOCK:     ACTUAL GPIO CONTROL OF THE LED PIN ON THE HARDWARE
+// GARAGEMOTOR: ACTUAL GPIO CONTROL OF THE 4066 IC THAT SHORTS THE GARAGE OPENER
+//***********************************/
 var v0 = new blynk.VirtualPin(0);
 var v1 = new blynk.VirtualPin(1);
 var v10 = new blynk.VirtualPin(10);
 var v11 = new blynk.VirtualPin(11);
+var v13 = new blynk.VirtualPin(13);
+var v12 = new blynk.VirtualPin(12);
+var v20 = new blynk.VirtualPin(20);
+var v21 = new blynk.VirtualPin(21);
 var ledLock = new Gpio(18, 'out');
 var garageMotor = new Gpio(17, 'out');
+
 var locked = true;
-var garageStatus = 'closed';
-var garageInMotion = false;
 var blinkProgress = 0;
 const blinkCount = 58;
 var date = new Date();
-var lastGarageMovement = 0;
+var garageState = {
+    CLOSED: 0,
+    OPENED: 1,
+    OPENING: 2,
+    CLOSING: 3,
+};
+/**
+ * BUILD THE GARAGE OBJECT
+ */
+var Garage = function(){
+    var self = this;
 
-(function checkDoor(prevMovement){
-    // log.info("checkdoor() called");
+    this.status = garageState.CLOSED;
+    this.inMotion = false;
+    this.position = 0;
+    this.ceiling = 0;
+    this.lastMovement = 0;
 
-    if (lastGarageMovement == prevMovement) {
-        blynk.virtualWrite(12, blinkProgress);
+    this.positionWatcher = function(lastMovement, timeout) {
+        var timeout = timeout || 500;
 
-        if (garageStatus == "closing" || garageStatus == "opening") {
-            log.error("Door stopped moving, but status is still " + garageStatus);
+        if(this.lastMovement == lastMovement) {
+            v12.write(this.position);
+        }
+        setTimeout(function(){
+            self.positionWatcher(this.lastMovement, timeout);
+        }, timeout);
+    };
+
+    this.activateIndicators = function() {
+        locked = true;
+        ledLock.writeSync(0);
+        v0.write(1);
+        v20.write(0);
+        v21.write(0);
+        this.inMotion = true;
+        if(this.status == garageState.CLOSED) {
+            this.status = garageState.OPENING;
+        } else {
+            this.status = garageState.CLOSING;
+        }
+        v13.write(centerText(garage.statusToString()));
+
+        // THIS SHOULD BE ADDED TO THE VIRTUAL AND PHYSICAL PINS
+        blink(blinkCount);
+    };
+
+    this.activateOpener = function(holdTime) {
+        var holdTime = holdTime || 200;
+    
+        // this.status += 2;
+        if (ARMED === true) {
+            garageMotor.writeSync(1);
+            setTimeout(function(){
+                garageMotor.writeSync(0);
+            }, holdTime);    
         }
     }
 
-    setTimeout(function(){
-        checkDoor(lastGarageMovement);
-    }, 500);
-})();
+    this.statusToString = function(state) {
+        var state = state || this.status;
+        var keyNames = Object.keys(garageState);
+        for (var i in keyNames) {
+            if (state == i) {
+                return keyNames[i];
+            }
+        }
+    }
+};
+
+var garage = new Garage();
 
 // THIS OPENS AND CLOSE THE GARAGE. BE CAREFUL!
 
 v1.on('write', function(param) {
     if(locked == false) {
         if (param[0] == 1) {
-            activateGarageIndicators();
-            pressGarage(500);
+            garage.activateIndicators();
+            garage.activateOpener(500);
         }
     }
 });
@@ -57,8 +132,9 @@ v1.on('write', function(param) {
 // THIS IS THE LOCK FOR THE GARAGE BUTTON
 
 v0.on('write', function(param) {
-    if (garageInMotion == true) {
-        blynk.virtualWrite(0, 1);
+    if (garage.inMotion == true) {
+        // blynk.virtualWrite(0, 1);
+        v0.write(1);
         return;
     }
     if(param[0] == 0) {
@@ -74,7 +150,7 @@ v0.on('write', function(param) {
 // Control Camera Exposure
 
 v10.on('write', function(param){
-    if (garageInMotion == true) {
+    if (garage.inMotion == true) {
         return;
     }
     var index = param[0];
@@ -94,7 +170,7 @@ v10.on('write', function(param){
         default:
             log.info("What happened?");
             log.info(index);
-            
+
             break;
     }
 });
@@ -107,54 +183,57 @@ v11.on('write', function(param){
     }
 });
 
-function pressGarage(timeout) {
-    if (typeof timeout == 'undefined') {
-        timeout = 200;
+function centerText(text, targetLength) {
+    var targetLength = targetLength || 16;
+    var difference = targetLength - text.length;
+    var returnText = "";
+
+    if (difference <= 0) {
+        return text;
     }
 
-    garageMotor.writeSync(1);
-    setTimeout(function(){
-        garageMotor.writeSync(0);
-    }, timeout);
-}
-
-function activateGarageIndicators() {
-    locked = true;
-    ledLock.writeSync(0);
-    blynk.virtualWrite(0, 1);
-    garageInMotion = true;
-    blink(blinkCount);
+    for (i=0; i<(difference/2); i++) {
+        returnText += " ";
+    }
+    returnText += text;
+    for (i=0; i<(difference/2); i++) {
+        returnText += " ";
+    }
+    return returnText;
 }
 
 function blink(count) {
 
-    if (garageStatus == "closing" ) {
+    if (garage.status == garageState.CLOSING) {
         blinkProgress = Math.round(100*(count/blinkCount));
-    } else if (garageStatus == "opening") {
+    } else if (garage.status == garageState.OPENING) {
         blinkProgress = Math.round(100-(100*(count/blinkCount)));
-    } 
-    // log.info("Door progress:" + blinkProgress);
-    blynk.virtualWrite(12, blinkProgress);
-
+    }
+    v13.write(centerText(garage.statusToString()));
+    // v13.write("0123456789ABCDEF012345");
+    v12.write(blinkProgress);
     if (count <= 0) {
         ledLock.write(0);
-        garageInMotion = false;
+        garage.inMotion = false;
         log.info("Blinker stopped.");
-        if(garageStatus == "opening") {
-            mouse.ceiling = mouse.y;
-            log.info("Garage is open with a ceiling of: " + mouse.ceiling);
-            blynk.virtualWrite(20, 255);
-            blynk.virtualWrite(21, 0);
-            garageStatus = "opened";
+        if(garage.status == garageState.OPENING) {
+            garage.ceiling = Math.max(garage.ceiling, garage.position);
+            // mouse.ceiling = mouse.y;
+            log.info("Garage is open with a ceiling of: " + garage.ceiling);
+            v20.write(255);
+            v21.write(0);
+            garage.status = garageState.OPENED;
         }
-        else if(garageStatus == "closing"){
-            mouse.y = 0;
+        if(garage.status == garageState.CLOSING) {
+            garage.position = 0;
+            // mouse.y = 0;
             log.info("Garage has closed.");
-            blynk.virtualWrite(20, 0);
-            blynk.virtualWrite(21, 255);
-            garageStatus = "closed";
+            v20.write(0);
+            v21.write(255);
+            garage.status = garageState.CLOSED;
         }
-        blynk.virtualWrite(13, garageStatus);
+        v13.write(centerText(garage.statusToString()));
+        // blynk.virtualWrite(13, garageStatus);
 
         return;// garageMotor.unexport();
     }
@@ -163,7 +242,7 @@ function blink(count) {
         if (err) {
           throw err;
         }
-    
+
         ledLock.write(value ^ 1, function (err) { // Asynchronous write.
           if (err) {
             throw err;
@@ -178,28 +257,33 @@ function blink(count) {
 
 mouse.on('move', function(data) {
     if (data.code == 1) {
-        if (garageInMotion == false) {
+
+        // WATCH FOR LOCAL GARAGE ACTIVATION
+        if (garage.inMotion == false) {
             blynk.notify("Somebody activated the garage!");
-            activateGarageIndicators();
+            log.info("Somebody activated the garage!");
+            garage.activateIndicators();
+
+            if ((data.value + garage.position) < garage.position) {
+                // LESS THAN MEANS THE GARAGE IS CLOSING
+                if(garage.status != garageState.CLOSING) {
+                    // THIS INDICATES THAT THE GARAGE IS ACTUALLY CLOSING, 
+                    // THOUGH THE SYSTEM THINKS IT IS OPENING
+                    garage.status = garageState.CLOSING;
+                }
+            } else {
+                // GREATER THAN MEANS THE GARAGE IS OPENING
+                if(garage.status != garageState.OPENING) {
+                    // THIS INDICATES THAT THE GARAGE IS ACTUALLY OPENING, 
+                    // THOUGH THE SYSTEM THINKS IT IS CLOSING
+                    garage.status = garageState.CLOSING;
+                }
+            }    
         }
-        if ((data.value + mouse.y) < mouse.y) {
-            if(garageStatus != "closing") {
-                garageStatus = "closing";
-                blynk.virtualWrite(20, 0);
-                blynk.virtualWrite(21, 0);
-                blynk.virtualWrite(13, garageStatus);
-            }
-        } else {
-            if(garageStatus != "opening") {
-                garageStatus = "opening";
-                blynk.virtualWrite(20, 0);
-                blynk.virtualWrite(21, 0);
-                blynk.virtualWrite(13, garageStatus);
-            }
-        }
-        mouse.y += data.value;
-        log.info("mouse.y = " + mouse.y);
-        lastGarageMovement = date.getMilliseconds();
+
+        garage.position += data.value;
+        log.info("garage position = " + garage.position);
+        garage.lastMovement = date.getMilliseconds();
     }
     // log.info("mouse y=" + mouse.y);
     // cmd.run('echo "X:' + mouse.x + ' :: Y: ' + mouse.y + '" >> sesame.log');
